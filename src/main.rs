84e837775b5ebe8883b839a66c97ee1de62a143c
Brian Davenport <bdav87@gmail.com>
2019-08-16 06:31:58 -0500

    Multiple messages from 2 spawned threads

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,32 @@ use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
 
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
             String::from("from"),
             String::from("the"),
-            String::from("thread"),
+            String::from("clone"),
+        ];
+
+        for val in vals {
+            let outbound = tx1.send(val);
+            match outbound {
+                Ok(val) => val,
+                Err(e) => panic!("Could not send msg: {:?}", e),
+            };
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("this"),
+            String::from("message"),
+            String::from("from"),
+            String::from("original"),
         ];
 
         for val in vals {
