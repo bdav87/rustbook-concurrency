442da88e2ee4a48bdb8486de7f7586ba4bb86754
Brian Davenport <bdav87@gmail.com>
2019-08-17 07:26:27 -0500

    Basic Mutex example

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,12 @@
-use std::thread;
-use std::sync::mpsc;
-use std::time::Duration;
+use std::sync::Mutex;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let tx1 = mpsc::Sender::clone(&tx);
+    let m = Mutex::new(5);
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("clone"),
-        ];
-
-        for val in vals {
-            let outbound = tx1.send(val);
-            match outbound {
-                Ok(val) => val,
-                Err(e) => panic!("Could not send msg: {:?}", e),
-            };
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("this"),
-            String::from("message"),
-            String::from("from"),
-            String::from("original"),
-        ];
-
-        for val in vals {
-            let outbound = tx.send(val);
-            match outbound {
-                Ok(val) => val,
-                Err(e) => panic!("Could not send msg: {:?}", e),
-            };
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
+
+    println!("m = {:?}", m);
 }
